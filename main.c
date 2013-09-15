ad307ac8aeae3749c4ed12f710b1ff50e0fc142a
Satius <m-burakov@ya.ru>
2013-09-15 19:55:18 +0400

    Try out xcb

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <X11/Xlib.h>
+/*#include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/xpm.h>
 
@@ -93,5 +93,71 @@ int main(int argc, char** argv)
   }
 
   XCloseDisplay(display);
+  return 0;
+}*/
+
+#include <xcb/xcb.h>
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+  xcb_window_t         w;
+  xcb_generic_event_t *e;
+  uint32_t             mask;
+  uint32_t             values[2];
+  int                  done = 0;
+  xcb_rectangle_t      r = { 20, 20, 60, 60 };
+ 
+  xcb_connection_t* c = xcb_connect(NULL, NULL);
+  if (xcb_connection_has_error(c))
+  {
+    printf("Cannot open display\n");
+    exit(1);
+  }
+  
+  // Get first screen
+  xcb_screen_t* s = xcb_setup_roots_iterator(xcb_get_setup(c)).data;
+
+  // Create context
+  xcb_gcontext_t g = xcb_generate_id(c);
+  w = s->root;
+  mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
+  values[0] = s->black_pixel;
+  values[1] = 0;
+  xcb_create_gc(c, g, w, mask, values);
+
+  // Create window
+  w = xcb_generate_id(c);
+  mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+  values[0] = s->white_pixel;
+  values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
+  xcb_create_window(c, s->root_depth, w, s->root,
+                    120, 120, 100, 100, 1,
+                    XCB_WINDOW_CLASS_INPUT_OUTPUT, s->root_visual,
+                    mask, values);
+ 
+  // Show window
+  xcb_map_window(c, w);
+  xcb_flush(c);
+ 
+  while (!done && (e = xcb_wait_for_event(c)))
+  {
+    switch (e->response_type & ~0x80)
+    {
+    case XCB_EXPOSE:
+      // Draw or repaint
+      xcb_poly_fill_rectangle(c, w, g,  1, &r);
+      xcb_flush(c);
+      break;
+    case XCB_KEY_PRESS:
+      // Exit on keypress
+      done = 1;
+      break;
+    }
+    free(e);
+  }
+
+  xcb_disconnect(c);
   return 0;
 }
